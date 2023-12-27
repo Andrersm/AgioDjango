3ae71cdf74ef6b49200d3b85ca4788eec440236f
Andre Morais <andre.mv.2@hotmail.com>
2023-12-27 19:48:49 -0300

    testeHttp

--- project/settings.py
+++ project/settings.py
@@ -136,7 +136,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
-
+CORS_ALLOWED_ORIGINS = [
+    "http://34.71.44.168/",
+]
 try:
     from project.local_settings import *
 except ImportError:
